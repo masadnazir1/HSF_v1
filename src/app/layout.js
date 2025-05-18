e085d2df728000c34170a0d0532e80dad5dff828
masadnazir1 <masadnazir1@gmail.com>
2025-05-18 16:51:18 +0500

    added the Clarity code  --asad

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import Footer from "./Footer/Footer";
 import Header from "./Header/NavBar";
 import "./globals.css";
+import Script from "next/script";
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 
 const geistSans = Geist({
@@ -23,6 +24,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Script
+          id="clarity-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "rlh0wj00hn");
+            `,
+          }}
+        />
         <Header />
         <div>{children}</div>
         <Footer />
